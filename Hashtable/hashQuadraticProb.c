4e129855f693142e10f3941dc8a2d1ec9e1fe42f
Mohammed Hassan <mh1921544@gmail.com>
2023-10-27 13:35:35 +0530

    Hashing

---
+++ Hashtable/hashQuadraticProb.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+
+typedef struct item
+{
+    int key, marks;
+} item;
+
+typedef struct hashtable
+{
+    int flag;
+    item *data;
+} hashtable;
+hashtable *array;
+
+void init()
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        array[i].flag = 0;
+        array[i].data = NULL;
+    }
+}
+
+int hash_function(int key)
+{
+    return (key % MAX);
+}
+
+void insert(int key, int value)
+{
+    item *ele = (item *)malloc(sizeof(item));
+    ele->key = key;
+    ele->marks = value;
+    int i, index;
+    i = hash_function(key);
+    index = i;
+    int k = 1;
+    while (array[i].flag == 1)
+    {
+        if (array[i].data->key == key)
+        {
+            printf("Duplicate, !! update marks\n");
+            array[i].data->marks = value;
+            return;
+        }
+        i = (index +(k*k)) % MAX;
+        if (i == index)
+        {
+            printf("Hash table is full\n");
+            return;
+        }
+        k++;
+    }
+    array[i].flag = 1;
+    array[i].data = ele;
+}
+
+void delete(int key)
+{
+    int index, i;
+    i = hash_function(key);
+    index = i;
+    int k = 1;
+    while (array[i].flag == 1)
+    {
+        if (array[i].data->key == key)
+        {
+            array[i].flag = 0;
+            array[i].data = NULL;
+            printf("Key deleted\n");
+            return;
+        }
+        i = (i + (k*k)) % MAX;
+        if (i == index)
+            break;
+        k++;
+    }
+    printf("Key not found\n");
+}
+
+void search(int key)
+{
+    int index, i;
+    i = hash_function(key);
+    index = i;
+    int k = 1;
+    while (array[i].flag == 1)
+    {
+        if (array[i].data->key == key)
+        {
+            printf("Key found at index %d\n", i);
+            return;
+        }
+        i = (index + (k*k)) % MAX;
+        if (i == index)
+            break;
+        k++;
+    }
+    printf("Key not found\n");
+}
+
+void display()
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        if (array[i].flag == 0)
+            printf("array[%d] has no elements\n", i);
+        else 
+            printf("array[%d] has elements -:  Key: %d\tMarks: %d\n", i, array[i].data->key, array[i].data->marks);
+    }
+}
+
+int main()
+{
+    int key, value, ch;
+    array = (hashtable *)malloc(MAX * sizeof(hashtable));
+    init();
+    while (1)
+    {
+        printf("1. Insertion\t2. Deletion\t3. Display\t4. Search\t5. Exit\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("Enter the key value: ");
+            scanf("%d", &key);
+            printf("Enter the marks: ");
+            scanf("%d", &value);
+            insert(key, value);
+            break;
+        case 2:
+            printf("Enter the key value to be deleted: ");
+            scanf("%d", &key);
+            delete (key);
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            printf("Enter the key value to be searched: ");
+            scanf("%d", &key);
+            search(key);
+            break;
+        case 5:
+            exit(0);
+        default:
+            printf("Invalid choice\n");
+        }
+    }
+}
